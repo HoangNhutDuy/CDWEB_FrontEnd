c734d2c7a84b13f51506d3ed51a840bd0335a799
HoangNhutDuy <20130238@st.hcmuaf.edu.vn>
2024-06-23 11:48:41 +0700

    complete get all user and delete user

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css"
 
 import routes from "./routes"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 function App() {
   return (
     <Router>
@@ -9,9 +11,12 @@ function App() {
         <Routes>
           {routes.map((route) => {
             const Page = route.Component
-            return <Route key={route.path} path={route.path} element={<Page />} />
+            return (
+              <Route key={route.path} path={route.path} element={<Page />} />
+            )
           })}
         </Routes>
+        <ToastContainer />
       </div>
     </Router>
   )
