a3b5943716f97d846c064904b4233ae5f92f90f7
HoangNhutDuy <20130238@st.hcmuaf.edu.vn>
2024-06-23 18:18:42 +0700

    fix bug

--- src/components/admin/TableContent/TableContentProduct.jsx
+++ src/components/admin/TableContent/TableContentProduct.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"
 import Table from "@mui/material/Table"
 import TableBody from "@mui/material/TableBody"
 import TableCell from "@mui/material/TableCell"
@@ -10,18 +10,17 @@ import Avatar from "@mui/material/Avatar"
 import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined"
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined"
 import { ToastContainer, toast } from "react-toastify"
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css"
 import ModalUpdateProduct from "../Modal/ModalUpdateProduct"
-import ModalProduct from "../Modal/ModalProduct";
-import axios from "axios"; 
-const TableContentProduct = ({ productId ,productData ,fetchProducts}) => {
-
+import ModalProduct from "../Modal/ModalProduct"
+import axios from "axios"
+const TableContentProduct = ({ productId, productData, fetchProducts }) => {
   const [open, setOpen] = React.useState(false)
-  
+
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
-  const [error, setError] = useState('');
-  const [products, setProducts] = useState([]);
+  const [error, setError] = useState("")
+  const [products, setProducts] = useState([])
   // const [newProducts, setNewProducts] = useState({
   //   idProduct: productId,
   //   name: "",
@@ -34,61 +33,56 @@ const TableContentProduct = ({ productId ,productData ,fetchProducts}) => {
   // });
   // const [newProducts, setNewProducts] = useState([]);
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value } = e.target
     setProducts((prevProduct) => ({
       ...prevProduct,
       [name]: value,
-    }));
-  };
+    }))
+  }
   useEffect(() => {
-
-    const storedProducts = JSON.parse(localStorage.getItem("products"));
+    const storedProducts = JSON.parse(localStorage.getItem("products"))
     if (storedProducts) {
-      setProducts(storedProducts);
+      setProducts(storedProducts)
     } else {
-      fetchProductsFromDatabase();
+      fetchProductsFromDatabase()
     }
-  }, []);
-
+  }, [])
 
   const fetchProductsFromDatabase = async () => {
     try {
-      const response = await axios.get("http://localhost:8080/product/getAll");
+      const response = await axios.get("http://localhost:8080/product/getAll")
 
-       const fetchedProducts = response.data.slice(0, 10); 
-      setProducts(fetchedProducts);
+      const fetchedProducts = response.data.slice(0, 10)
+      setProducts(fetchedProducts)
 
-      localStorage.setItem("products", JSON.stringify(fetchedProducts));
+      localStorage.setItem("products", JSON.stringify(fetchedProducts))
     } catch (error) {
-      console.error("Error fetching products:", error);
+      console.error("Error fetching products:", error)
     }
-  };
-      
+  }
+
   useEffect(() => {
-    fetchProductsFromDatabase();
-  }, []);
+    fetchProductsFromDatabase()
+  }, [])
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
     try {
       const response = await axios.post(
         "http://localhost:8080/product/add",
         products
-      );
-      console.log("Product added:", response?.data);
+      )
+      console.log("Product added:", response?.data)
       // setNewProducts([...newProducts, response.data]); // Cập nhật state cho sản phẩm mới
-      setProducts([...products,response.data]); 
-      
-     
+      setProducts([...products, response.data])
 
-      handleClose(); // Close modal after successful submission
-      setError(''); // Fetch updated products after adding
-
+      handleClose() // Close modal after successful submission
+      setError("") // Fetch updated products after adding
     } catch (error) {
-      console.error("Error adding product:", error);
+      console.error("Error adding product:", error)
       // Handle error, show an error message, etc.
     }
-  };
+  }
   const showToastSuccess = (message) => {
     if (!message) {
       return
@@ -107,258 +101,276 @@ const TableContentProduct = ({ productId ,productData ,fetchProducts}) => {
     })
   }
   const handleAddProduct = async (newProduct) => {
-
-    
     try {
-      const response = await axios.post("http://localhost:8080/product/add", newProduct);
-      const addedProduct = response.data;
-  
-   
-      setProducts((prevProducts) => [addedProduct, ...prevProducts]);
-  
-  
-      const updatedProducts = [addedProduct, ...products];
-      localStorage.setItem("products", JSON.stringify(updatedProducts));
-  
-      
-  
-    
-      toast.success("Product added successfully!");
+      const response = await axios.post(
+        "http://localhost:8080/product/add",
+        newProduct
+      )
+      const addedProduct = response.data
 
-    
+      setProducts((prevProducts) => [addedProduct, ...prevProducts])
+
+      const updatedProducts = [addedProduct, ...products]
+      localStorage.setItem("products", JSON.stringify(updatedProducts))
+
+      toast.success("Product added successfully!")
     } catch (error) {
-      console.error("Error adding product:", error);
-      toast.error("Failed to add product. Please try again.");
+      console.error("Error adding product:", error)
+      toast.error("Failed to add product. Please try again.")
     }
-  };
+  }
   const handleDelete = (productId) => {
-    axios.delete(`http://localhost:8080/product/${productId}`)
+    axios
+      .delete(`http://localhost:8080/product/${productId}`)
       .then((response) => {
-        console.log("Product deleted successfully");
-        setProducts(products.filter(product => product.id !== productId));
-        const updatedProducts = products.filter(product => product.id !== productId);
-        localStorage.setItem("products", JSON.stringify(updatedProducts));
+        console.log("Product deleted successfully")
+        setProducts(products.filter((product) => product.id !== productId))
+        const updatedProducts = products.filter(
+          (product) => product.id !== productId
+        )
+        localStorage.setItem("products", JSON.stringify(updatedProducts))
       })
       .catch((error) => {
-        console.error("Error deleting product:", error);
-      });
-  };
+        console.error("Error deleting product:", error)
+      })
+  }
 
-   
   return (
-<div>
-   
-    <TableContainer component={Paper}>
-      
-    <ModalProduct onAddProduct={handleAddProduct} />
-      <Table
-        sx={{
-          minWidth: 650,
-          gap: 2,
-          borderCollapse: "separate",
-          borderSpacing: "1em 1em",
-          backgroundColor: "white",
-        }}
-        size="small"
-        aria-label="a dense table"
-      >
-        <TableHead>
-          <TableRow>
-            <TableCell
-              sx={{
-                textAlign: "center",
-                color: "#ACACAC",
-                borderBottom: "none",
-                fontSize: "14px",
-              }}
-            >
-              ID
-            </TableCell>
-            <TableCell
-              sx={{ color: "#ACACAC", borderBottom: "none", fontSize: "14px" }}
-              align="left"
-            >
-              Tên sản phẩm
-            </TableCell>
-            <TableCell
-              sx={{ color: "#ACACAC", borderBottom: "none", fontSize: "14px" }}
-              align="left"
-            >
-              Hình ảnh
-            </TableCell>
-            <TableCell
-              sx={{ color: "#ACACAC", borderBottom: "none", fontSize: "14px" }}
-              align="left"
-            >
-              Brand
-            </TableCell>
-            <TableCell
-              sx={{ color: "#ACACAC", borderBottom: "none", fontSize: "14px" }}
-              align="left"
-            >
-              Model
-            </TableCell>
-            <TableCell
-              sx={{
-                color: "#ACACAC",
-                borderBottom: "none",
-                width: "350px",
-                fontSize: "14px",
-              }}
-              align="left"
-            >
-              Mô tả
-            </TableCell>
-            <TableCell
-              sx={{
-                color: "#ACACAC",
-                borderBottom: "none",
-                width: "350px",
-                fontSize: "14px",
-              }}
-              align="left"
-            >
-              Giá
-            </TableCell>
-            <TableCell
-              sx={{ color: "#ACACAC", borderBottom: "none", fontSize: "14px" }}
-              align="left"
-            >
-              Số lượng
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        {products?.length > 0 ? (
-          products?.map((row) => (
-            <TableRow
-              key={row?.id}
-              sx={{
-                height: "65px",
-                fontSize: "14px",
-              }}
-            >
-              <TableCell component="th" scope="row">
-                {row?.productId}
-              </TableCell>
+    <div>
+      <TableContainer component={Paper}>
+        <ModalProduct onAddProduct={handleAddProduct} />
+        <Table
+          sx={{
+            minWidth: 650,
+            gap: 2,
+            borderCollapse: "separate",
+            borderSpacing: "1em 1em",
+            backgroundColor: "white",
+          }}
+          size="small"
+          aria-label="a dense table"
+        >
+          <TableHead>
+            <TableRow>
               <TableCell
                 sx={{
-                  textAlign: "left",
+                  textAlign: "center",
+                  color: "#ACACAC",
                   borderBottom: "none",
-                  fontWeight: "600",
                   fontSize: "14px",
                 }}
               >
-                {row?.name}
+                ID
               </TableCell>
               <TableCell
                 sx={{
+                  color: "#ACACAC",
                   borderBottom: "none",
-                  fontWeight: "600",
                   fontSize: "14px",
                 }}
                 align="left"
               >
-                <img
-                  style={{ width: "100px", height: "100px", fontSize: "14px" }}
-                  src={row?.img}
-                  alt="product_img"
-                />
+                Tên sản phẩm
               </TableCell>
               <TableCell
                 sx={{
+                  color: "#ACACAC",
                   borderBottom: "none",
-                  fontWeight: "600",
                   fontSize: "14px",
                 }}
                 align="left"
               >
-                {row?.brand}
+                Hình ảnh
               </TableCell>
               <TableCell
                 sx={{
+                  color: "#ACACAC",
                   borderBottom: "none",
-                  fontWeight: "600",
                   fontSize: "14px",
                 }}
                 align="left"
               >
-                {row?.model}
+                Brand
               </TableCell>
-
               <TableCell
                 sx={{
+                  color: "#ACACAC",
                   borderBottom: "none",
-                  fontWeight: "600",
-                  color: "#FEAF00",
-                  cursor: "pointer",
                   fontSize: "14px",
                 }}
                 align="left"
               >
-                {row?.description}
+                Model
               </TableCell>
               <TableCell
                 sx={{
+                  color: "#ACACAC",
                   borderBottom: "none",
-                  fontWeight: "600",
-                  color: "#FEAF00",
-                  cursor: "pointer",
+                  width: "350px",
                   fontSize: "14px",
                 }}
                 align="left"
               >
-                {row?.price}
+                Mô tả
               </TableCell>
               <TableCell
                 sx={{
+                  color: "#ACACAC",
                   borderBottom: "none",
-                  fontWeight: "600",
-                  color: "#FEAF00",
-                  cursor: "pointer",
+                  width: "350px",
                   fontSize: "14px",
                 }}
                 align="left"
               >
-                {row?.stockQuantity}
+                Giá
               </TableCell>
               <TableCell
                 sx={{
+                  color: "#ACACAC",
                   borderBottom: "none",
-                  fontWeight: "600",
-                  color: "#FEAF00",
-                  cursor: "pointer",
+                  fontSize: "14px",
                 }}
                 align="left"
               >
-
-   
-                <DeleteOutlineOutlinedIcon sx={{ fontSize: "20px" }} onclick={() => handleDelete(row?.productId)} />
-              </TableCell>
-              <TableCell
-                sx={{
-                  borderBottom: "none",
-                  fontWeight: "600",
-                  color: "#FEAF00",
-                  cursor: "pointer",
-                }}
-                align="left"
-              >
-                <ModalUpdateProduct />
-                <ToastContainer />
+                Số lượng
               </TableCell>
             </TableRow>
-            ))
-             ) : (
+          </TableHead>
+          <TableBody>
+            {products?.length > 0 ? (
+              products?.map((row) => (
+                <TableRow
+                  key={row?.id}
+                  sx={{
+                    height: "65px",
+                    fontSize: "14px",
+                  }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row?.productId}
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      textAlign: "left",
+                      borderBottom: "none",
+                      fontWeight: "600",
+                      fontSize: "14px",
+                    }}
+                  >
+                    {row?.name}
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      borderBottom: "none",
+                      fontWeight: "600",
+                      fontSize: "14px",
+                    }}
+                    align="left"
+                  >
+                    <img
+                      style={{
+                        width: "100px",
+                        height: "100px",
+                        fontSize: "14px",
+                      }}
+                      src={row?.img}
+                      alt="product_img"
+                    />
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      borderBottom: "none",
+                      fontWeight: "600",
+                      fontSize: "14px",
+                    }}
+                    align="left"
+                  >
+                    {row?.brand}
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      borderBottom: "none",
+                      fontWeight: "600",
+                      fontSize: "14px",
+                    }}
+                    align="left"
+                  >
+                    {row?.model}
+                  </TableCell>
+
+                  <TableCell
+                    sx={{
+                      borderBottom: "none",
+                      fontWeight: "600",
+                      color: "#FEAF00",
+                      cursor: "pointer",
+                      fontSize: "14px",
+                    }}
+                    align="left"
+                  >
+                    {row?.description}
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      borderBottom: "none",
+                      fontWeight: "600",
+                      color: "#FEAF00",
+                      cursor: "pointer",
+                      fontSize: "14px",
+                    }}
+                    align="left"
+                  >
+                    {row?.price}
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      borderBottom: "none",
+                      fontWeight: "600",
+                      color: "#FEAF00",
+                      cursor: "pointer",
+                      fontSize: "14px",
+                    }}
+                    align="left"
+                  >
+                    {row?.stockQuantity}
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      borderBottom: "none",
+                      fontWeight: "600",
+                      color: "#FEAF00",
+                      cursor: "pointer",
+                    }}
+                    align="left"
+                  >
+                    <DeleteOutlineOutlinedIcon
+                      sx={{ fontSize: "20px" }}
+                      onclick={() => handleDelete(row?.productId)}
+                    />
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      borderBottom: "none",
+                      fontWeight: "600",
+                      color: "#FEAF00",
+                      cursor: "pointer",
+                    }}
+                    align="left"
+                  >
+                    <ModalUpdateProduct />
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
               <TableRow>
                 <TableCell colSpan={6} align="center">
                   No users found
                 </TableCell>
               </TableRow>
             )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   )
 }
